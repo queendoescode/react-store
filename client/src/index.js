74eb5e2eaa7cfcaa83d02bb329b9d62a9a903954
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-11-04 22:14:30 -0400

    added a Redux provider in place of Context API provider.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './utils/reducers';
+
+const preloadedState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+};
+
+const store = configureStore(
+    {
+      reducer,
+      preloadedState
+    }
+  );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
